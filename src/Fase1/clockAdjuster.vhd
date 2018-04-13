20ad31f562f43ea00d5b5a124ca8adc99ad1984d
gnmmarechal <mliberato@ua.pt>
2018-04-13 03:09:42 +0100

    Clock divider actually works now

--- src/Fase1/clockAdjuster.vhd
+++ src/Fase1/clockAdjuster.vhd
@@ -2,9 +2,9 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
-
 entity clockAdjuster is
-	generic(	K		:	positive := 50000000); -- Testado com k = 2, o clock fica com o período o dobro do original (50MHz para 25MHz)
+	-- Testado com k = 2 (clkOut é de 25MHz)
+	generic(	K		:	positive := 50000000); -- Divide o clock por 50*10^6, para obter um clock de 1Hz
 	port	 ( clkIn	:	in	std_logic;
 				clkOut:	out std_logic);
 end clockAdjuster;
@@ -15,11 +15,13 @@ begin
 	process(clkIn)
 	begin
 		if (rising_edge(clkIn)) then
-			if(s_counter = K-1) then
-				clkOut <= '1';
+			if(s_counter = K - 1) then
+				clkOut 	 <= '0';
 				s_counter <= 0;
 			else
-				clkOut <= '0';
+				if(s_counter = K/2 - 1) then
+					clkOut <= '1';
+				end if;
 				s_counter <= s_counter + 1;
 			end if;
 		end if;
